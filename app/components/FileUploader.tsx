488dcd099525584fcaae1e99a354b3ae00f7bc8b
Darshil Mukesh Desai <darshil.desai.201083@gmail.com>
2025-07-21 16:35:54 -0400

    PDF upload implemented

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
 import {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
+import {formatSize} from '../lib/utils'
 
 interface FileUploaderProps {
-    onFileSelect?: (dile:File | null) => void
+    onFileSelect?: (file:File | null) => void
 }
 
 
@@ -31,15 +32,27 @@ const FileUploader = ({onFileSelect}: FileUploaderProps) => {
             <div {...getRootProps()}>
                 <input {...getInputProps()} />
                 <div className="space-y-4 cursor-pointer">
-                    <div className="mx-auto w-16 h-16 flex items-center justify-center">
-                        <img src="/icons/info.svg" alt="upload" className="size-20" />
-                    </div>
                     {file ? (
-                        <div>
-
+                        <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
+                            <img src="/images/pdf.png" alt="pdf" className="size-10" />
+                            <div className="flex items-center space-x-3">
+                                <div>
+                                    <p className="text-sm font-medium text-gray-700 truncate max-w-xs">{file.name}</p>
+                                    <p className="text-sm text-gray-500">{formatSize(file.size)}</p>
+                                </div>
+                            </div>
+                            <button type="button" className="p-2 cursor-pointer" onClick={(e) => {
+                                e.stopPropagation();
+                                onFileSelect?.(null);
+                            }}>
+                                <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" />
+                            </button>
                         </div>
                     ) : (
                         <div>
+                            <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                                <img src="/icons/info.svg" alt="upload" className="size-20" />
+                            </div>
                             <p className="text-lg text-gray-500">
                                 <span className="font-semibold">
                                     Click to Upload
