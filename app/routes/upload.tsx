9d08cd978900d86fad781bf383662f8611883f57
Darshil Mukesh Desai <darshil.desai.201083@gmail.com>
2025-07-22 15:36:07 -0400

    Resume Feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -63,6 +63,9 @@ const Upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting...');
         console.log(data);
+
+        navigate(`/resume/${uuid}`);
+
     }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
